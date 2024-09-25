85999fc39fb601e57d2b6baf0286ca81489b3385
lchaconc <luis.chacon.campos@gmail.com>
2024-09-25 08:42:27 -0600

    imp de buscador

--- app/app/usuarios/page.jsx
+++ app/app/usuarios/page.jsx
@@ -1,34 +1,45 @@
 "use client";
-import  { useState, useEffect } from 'react';
-import TablaPaginada from '@/components/TablaPaginada';
+import { useState, useEffect } from "react";
+import TablaPaginada from "@/components/TablaPaginada";
+import BuscarPalabras from "@/components/BuscarPalabras";
 import endpoints from "@/_endpoints";
 import { getData } from "@/utils/data";
 
 export default function Usuarios() {
-
   const [usuarios, setUsuarios] = useState(null);
-
+  const [filtrados, setFiltrados] = useState(null);
 
   useEffect(() => {
     setup();
   }, []);
 
-  const setup = async ()=> {
+  useEffect(() => {
+    usuarios && setFiltrados(usuarios)
+  }, [usuarios]);
+
+  const setup = async () => {
     console.log(endpoints.getUsuarios);
-     setUsuarios(await getData(endpoints.getUsuarios));
-    
-    
-  } 
-
-
+    setUsuarios(await getData(endpoints.getUsuarios));    
+  };
 
   return (
-    <>
-    {
-      usuarios ? <TablaPaginada data={usuarios} recordsPerPage={5} /> : 
-      <div className="alert alert-info"> Cargando datos. Por favor espere... </div>
-    }
-    </>
-    
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1>Usuarios</h1>
+        </div>
+      </div>
+
+      <BuscarPalabras array={usuarios} setFiltrados={setFiltrados} />
+
+      {filtrados ? (
+        <TablaPaginada data={filtrados} recordsPerPage={5} />
+      ) : (
+        <div className="alert alert-info">
+          {" "}
+          Cargando datos. Por favor espere...{" "}
+        </div>
+      )}
+    </div>
   );
 }
---
+++ app/components/BuscarPalabras.jsx
@@ -0,0 +1,78 @@
+import { useRef } from 'react';
+
+
+export default function BuscarPalabras({ array, setFiltrados  }) {
+  //console.log("array desde el buscador", array);
+  const palabraRef = useRef(null);
+
+
+  const handleReset =()=> {
+    palabraRef.current.value=""
+    setFiltrados(array)
+  }
+
+  const quitarAcentos = (palabra) => {
+    return palabra.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  };
+
+  const handleObtenerPalabras = (e) => {
+    let palabras = e.target.value.toLowerCase();
+    palabras = palabras.split(" ");
+    buscarCoincidencias(palabras);
+  };
+
+  
+  function buscarCoincidencias(palabras) {
+    const resultados = [];
+
+    array.forEach((item) => {
+      const nombre = quitarAcentos(item.nombre.toLowerCase());
+      const apellido = quitarAcentos(item.apellido.toLowerCase());
+      const correo = quitarAcentos(item.correo.toLowerCase());
+
+      
+
+      const coincidencia = palabras.every((palabra) => {
+        return (
+          nombre.includes(quitarAcentos(palabra)) ||
+          apellido.includes(quitarAcentos(palabra)) ||
+          correo.includes(quitarAcentos(palabra))          
+        );
+      });
+
+      if (coincidencia) {
+        resultados.push(item);
+      }
+    });
+
+    setFiltrados(resultados);
+  }
+
+  return (
+    <div className="row">
+      <div className="col-sm-10">
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-default">
+            Búsqueda
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-default"
+            placeholder="Digite la palabra o parte de ella"
+            //onChange={handleObtenerPalabras}
+            onInput={handleObtenerPalabras}
+            ref={palabraRef}
+          />
+        </div>
+      </div>
+      <div className="col-sm-2 text-end">
+        <button
+        className='btn btn-outline-mep'
+        onClick={handleReset}
+        >Reiniciar</button>
+      </div>
+    </div>
+  );
+}
